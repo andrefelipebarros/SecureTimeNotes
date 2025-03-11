75bad70e546801ce7236a036544136a446e53fcb
André Felipe de Barros A. N. <100530782+andrefelipebarros@users.noreply.github.com>
2025-03-11 16:32:17 +0000

    init: Início do projeto.

---
+++ securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/SecuretimenotesApplication.java
@@ -0,0 +1,13 @@
+package securetimenotes.andrefelipebarros.securetimenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuretimenotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuretimenotesApplication.class, args);
+	}
+
+}
---
+++ securetimenotes/src/test/java/securetimenotes/andrefelipebarros/securetimenotes/SecuretimenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package securetimenotes.andrefelipebarros.securetimenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuretimenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
