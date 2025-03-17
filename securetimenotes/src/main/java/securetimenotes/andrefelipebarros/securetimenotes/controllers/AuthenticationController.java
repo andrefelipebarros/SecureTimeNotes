336e8c665a4d7f3eb83bcd8e21bf439ac32e9197
Andre Felipe B. A. Nogueira <afbarrosan@gmail.com>
2025-03-17 18:13:22 -0300

    feat(auth): Adding Automatic UserRole.
    Português: Estou atribuindo uma role padrão automaticamente. OBS: Caso não seja especificada previamente, a role será registrada como 'null' no banco de dados.
    
    English: I am automatically assigning a default role. Note: If not specified beforehand, the role will be saved as 'null' in the database.

--- securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/controllers/AuthenticationController.java
+++ securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/controllers/AuthenticationController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import securetimenotes.andrefelipebarros.securetimenotes.model.user.User;
+import securetimenotes.andrefelipebarros.securetimenotes.model.user.UserRole;
 import securetimenotes.andrefelipebarros.securetimenotes.model.user.body.AuthenticationDTO;
 import securetimenotes.andrefelipebarros.securetimenotes.model.user.body.LoginResponseDTO;
 import securetimenotes.andrefelipebarros.securetimenotes.model.user.body.RegisterDTO;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,6 +23,7 @@ import securetimenotes.andrefelipebarros.securetimenotes.repository.UserReposito
 
 @RestController
 @RequestMapping("auth")
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -33,12 +36,15 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<String> postMethodRegister(@RequestBody @Valid RegisterDTO data){
-        if(repository.findByUsername(data.username()) != null) return ResponseEntity.badRequest().body("Username already taken.");
+
+        if(repository.findByUsername(data.username()) != null) return ResponseEntity
+        .badRequest().body("Username already taken.");
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        User newUser = new User(data.username(), encryptedPassword, data.role());
-        this.repository.save(newUser);
+        UserRole role = (data.role() == null) ? UserRole.USER : data.role();
 
+        User newUser = new User(data.username(), encryptedPassword, role);
+        this.repository.save(newUser);
         return ResponseEntity.ok("User registered successfully.");
     }
 
