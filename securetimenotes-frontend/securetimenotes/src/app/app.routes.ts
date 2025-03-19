a8c0bf6e3d600636261f9c8bf75a118e7e08b49d
Andre Felipe B. A. Nogueira <afbarrosan@gmail.com>
2025-03-19 00:13:20 -0300

    feat: Organize project routes
    português-> Nesse commit, você organizou as rotas do seu aplicativo Angular, incluindo o redirecionamento para a página inicial (/home), além de configurar as rotas para os componentes HomeComponent e RegisterComponent para as páginas respectivas.
    
    english-> In this commit, you organized the routes of your Angular application, including the redirection to the home page (/home), as well as configuring the routes for the HomeComponent and RegisterComponent to their respective pages.
    
    Esses commits seguem a convenção feat porque ambos envolvem a adição ou organização de recursos importantes no seu projeto Angular.

---
+++ securetimenotes-frontend/securetimenotes/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path:'home', component: HomeComponent},
+
+    // Authentication
+    {path:'register', component: RegisterComponent},
+];
