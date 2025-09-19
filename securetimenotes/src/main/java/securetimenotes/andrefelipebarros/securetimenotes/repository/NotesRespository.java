a1e029ce8383a3751c52280da5d55ed7c06baaac
Andre Felipe de Barros Azevedo Nogueira <andre.nogueira@bndes.gov.br>
2025-09-19 14:16:45 -0300

    fix: Add Annotation to repositories.

--- securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/repository/NotesRespository.java
+++ securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/repository/NotesRespository.java
@@ -3,9 +3,12 @@ package securetimenotes.andrefelipebarros.securetimenotes.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import securetimenotes.andrefelipebarros.securetimenotes.model.note.Note;
 import securetimenotes.andrefelipebarros.securetimenotes.model.user.User;
 
+@Repository
 public interface NotesRespository extends JpaRepository<Note, Long>{
     List<Note> findByUser(User user);
 }
--- securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/repository/UserRepository.java
+++ securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/repository/UserRepository.java
@@ -2,11 +2,11 @@ package securetimenotes.andrefelipebarros.securetimenotes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import securetimenotes.andrefelipebarros.securetimenotes.model.user.User;
 
-
-
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     UserDetails findByUsername(String username);
 }
