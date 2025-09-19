367254c3b44643feb27ec26bef622d899feb0989
Andre Felipe de Barros Azevedo Nogueira <andre.nogueira@bndes.gov.br>
2025-09-19 14:57:56 -0300

    test: Remove false metrics

--- securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/SecuretimenotesApplication.java
+++ securetimenotes/src/main/java/securetimenotes/andrefelipebarros/securetimenotes/SecuretimenotesApplication.java
@@ -3,11 +3,11 @@ package securetimenotes.andrefelipebarros.securetimenotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class)
+@SpringBootApplication //(exclude = org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class)
 public class SecuretimenotesApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("management.metrics.binders.processor.enabled", "false");
+		//System.setProperty("management.metrics.binders.processor.enabled", "false");
 		SpringApplication.run(SecuretimenotesApplication.class, args);
 	}
 
